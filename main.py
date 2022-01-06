513b42637d7163c3c2a167604e57c5f27bf138d4
TERAKOSHITAISUKE <denpsyadvance@gmail.com>
2022-01-06 19:00:36 +0900

    re install numpy

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from bs4 import BeautifulSoup
 import streamlit as st
 import pandas as pd
 
+@st.cache
+def get_data(url):
+    res = requests.get(url)
 
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # elems = soup.select('[class="categoryArchiveItemTitle"]')
+    # print(elems[1])
+
+    article = soup.find_all(href=re.compile("article/"))
+    # print(article)
+
+    list = []
+    for el in article:
+        try:
+            list.append(el.contents[3].text)
+            # print("-"*10)
+        except:
+            break
 url = 'https://www.hokkaido-np.co.jp/news/n_economy'
-res = requests.get(url)
-
-soup = BeautifulSoup(res.text, 'html.parser')
-elems = soup.select('[class="categoryArchiveItemTitle"]')
-# print(elems[1])
-
-article = soup.find_all(href=re.compile("article/"))
-# print(article)
-
-list = []
-for el in article:
-    try:
-        list.append(el.contents[3].text)
-        # print("-"*10)
-    except:
-        break
     
 st.write('''
-        # 北海道新聞
+        # 北海道新聞[経済]
         ''')
 
 df = pd.DataFrame({'最新情報':list})
-df = df.style.hide_index()
 st.dataframe(df)
